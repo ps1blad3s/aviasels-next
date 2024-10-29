55e8ac6100a1bdb45c499a925f9ccf27325939f0
qwertylang <Vladik1122333@gmail.com>
2024-10-29 06:14:03 +0400

    refactoring, fix padding profileText

--- src/pages/homepage/HomePage.tsx
+++ src/pages/homepage/HomePage.tsx
@@ -8,21 +8,21 @@ const HomePage: React.FC = () => {
   return (
     <>
       <div className={styles.backgroundContainer}>
-        <Header />
-        <div style={{ padding: "20px" }}>
-          <FlightForm />
-        </div>
-        <div className={styles.container}>
-            <div className={styles.headingText}>
-                Горячие билеты
-            </div>
-          {flightCards.map((cardProps, index) => (
-            <>
-              <div className={styles.flightCard}>
-                <FlightCard key={index} {...cardProps} />
-              </div>
-            </>
-          ))}
+        <div className={styles.backgroundContainerTwo}>
+          <Header />
+          <div style={{ padding: "20px" }}>
+            <FlightForm />
+          </div>
+          <div className={styles.container}>
+            <div className={styles.headingText}>Горячие билеты</div>
+            {flightCards.map((cardProps, index) => (
+              <>
+                <div className={styles.flightCard}>
+                  <FlightCard key={index} {...cardProps} />
+                </div>
+              </>
+            ))}
+          </div>
         </div>
       </div>
     </>
